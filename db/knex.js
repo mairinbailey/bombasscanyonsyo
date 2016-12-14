c96f4c89f0f94acd63df109d52a95b38a64845e2
Mairin Bailey <mairin.bailey@gmail.com>
2016-12-14 15:01:41 -0700

    refactoring to RESTful API/CRUD routes

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,7 @@
+// environment is either production (node_env) OR development
 var environment= process.env.NODE_ENV || "development";
+// object that's hanging off the key of the environment that I'm in
 var config = require('../knexfile')[environment];
+//requiring knex module invoking it with config
 var knex = require('knex')(config);
 module.exports = knex;
--- routes/index.js
+++ routes/index.js
@@ -2,46 +2,65 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+// List all canyons
+router.get('/canyon', function(req, res, next) {
   knex('canyons').select().then(function (canyons) {
     res.render('index', {canyons: canyons});
   });
 });
 
-router.get('/create', function(req, res, next) {
+// Render create page/form
+router.get('/canyon/create', function(req, res, next) {
   res.render('create');
 });
 
-router.get('/:id', function(req, res, next) {
+// Display single canyon by id
+router.get('/canyon/:id', function(req, res, next) {
   knex('canyons').where({id: req.params.id}).first().then(function(canyon) {
     res.render('detail', {canyon: canyon});
   });
 });
 
-router.get('/:id/edit', function(req, res, next) {
+//Display edit page for single canyon
+router.get('/canyon/:id/edit', function(req, res, next) {
   knex('canyons').where({id: req.params.id}).first().then(function(canyon) {
     res.render('edit', {canyon: canyon});
   });
 });
 
-router.post('/:id/edit', function(req, res, next) {
-  knex('canyons').where({id:req.params.id}).update(req.body).then(function(canyon) {
-    res.redirect('/' + req.params.id);
-  });
-});
+//Send put request to update single canyon
+router.put('/canyon/:id', function(req,res,next){
+  knex('canyons').where({id: req.params.id}).update(req.body).then(function(){
+    res.redirect('/canyon/' + req.params.id)
+  })
+})
+
+// Update post using post instead of put/method override
+// router.post('/canyon/:id/edit', function(req, res, next) {
+//   knex('canyons').where({id:req.params.id}).update(req.body).then(function(canyon) {
+//     res.redirect('/canyon/' + req.params.id);
+//   });
+// });
 
 
+// Delete post using get instead of delete/method override
+// router.get('/canyon/:id/delete', function(req,res,next) {
+//   knex('canyons').where({id: req.params.id}).del().then(function() {
+//     res.redirect('/canyon');
+//   });
+// });
 
-router.get('/:id/delete', function(req,res,next) {
-  knex('canyons').where({id: req.params.id}).del().then(function() {
-    res.redirect('/');
-  });
-});
+//Delete single canyon and redirect home using put request and method override
+router.delete('/canyon/:id', function(req,res,next){
+  knex('canyons').where({id: req.params.id}).del().then(function(){
+    res.redirect('/canyon');
+  })
+})
 
-router.post('/create', function(req, res, next) {
-  knex('canyons').insert(req.body).then(function() {
-    res.redirect('/');
+//Create new canyon and load it's single canyon view
+router.post('/canyon/create', function(req, res, next) {
+  knex('canyons').insert(req.body).returning('id').then(function(id) {
+    res.redirect('/canyon/'+ id);
   }).catch(function(err) {
     console.log(err);
     next(err);
